f11f3280a52a05632821d57bff70353a56987de2
taylor.wtf <109391572+taylorwtf@users.noreply.github.com>
2023-03-14 21:28:32 -0700

    Update index.tsx
    delete  "import { Layout, Text, Page } from '@vercel/examples-ui'"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
-import { Layout, Text, Page } from '@vercel/examples-ui'
+
 import { Chat } from '../components/Chat'
 
 function Home() {
   return (
     <Page className="flex flex-col gap-12">
       <section className="flex flex-col gap-6">
-        <Text variant="h1">OpenAI GPT-3 text model usage example</Text>
+        <Text variant="h1">WTFI CHAD GPT-5</Text>
         <Text className="text-zinc-600">
-          In this example, a simple chat bot is implemented using Next.js, API
-          Routes, and OpenAI API.
+          WTF LETS GO!
         </Text>
       </section>
 
